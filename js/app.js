0d61ad67b9c7b4e9fc4251508628742bdf99dcdd
juancsalazaro <camilosala55zar@gmail.com>
2024-03-06 17:42:42 -0500

    slider

--- js/app.js
+++ js/app.js
@@ -17,7 +17,6 @@ function cambiarEstado(silla) {
   }
 }
 
-
 function toggleElemento() {
   if (elemento.style.display === "none") {
     elemento.style.display = "block";
@@ -25,3 +24,28 @@ function toggleElemento() {
     elemento.style.display = "none";
   }
 }
+
+const progressCircle = document.querySelector(".autoplay-progress svg");
+const progressContent = document.querySelector(".autoplay-progress span");
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  on: {
+    autoplayTimeLeft(s, time, progress) {
+      progressCircle.style.setProperty("--progress", 1 - progress);
+      progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+    },
+  },
+});
